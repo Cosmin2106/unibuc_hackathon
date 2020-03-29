38864c73c6aff4127ce865eba4139d31b5645724
Cosmin Aprodu <cosmin.aprodu@gmail.com>
2020-03-30 01:45:25 +0300

    Detalii

--- Site UB Hackathon/js/scroll.js
+++ Site UB Hackathon/js/scroll.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#navbar_logo").on('click', smoothScroll);
-  $("#signup_btn_main").on('click', smoothScroll);
+  $("#signup_btn_main").on('click', smoothScroll).on('click', changeColors);
   $("#navbar_opt1").on('click', smoothScroll);
   $("#navbar_opt2").on('click', smoothScroll);
 });
@@ -15,4 +15,9 @@ function smoothScroll() {
     }, 800, 'swing');
     window.location.hash = oldHash - mainNavHeight;
   }
+}
+
+function changeColors() {
+  $('#color-text-despre').addClass("text-col-blue");
+  $('#color-text-vs').addClass("text-col-yellow");
 }
