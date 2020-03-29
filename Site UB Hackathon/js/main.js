afc98aecf19803722dfe4d059ad7fa83a71ee9ee
Cosmin Aprodu <cosmin.aprodu@gmail.com>
2020-03-29 12:40:15 +0300

    Update footer

--- Site UB Hackathon/js/main.js
+++ Site UB Hackathon/js/main.js
@@ -9,3 +9,16 @@ function scrollNavbar() {
     document.getElementById("navbar_main").classList.remove("navbar-contract");
   }
 }
+
+$("navbar_logo").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function() {
+           window.location.hash = hash;
+      });
+  }
+});
--- Site UB Hackathon/js/scroll.js
+++ Site UB Hackathon/js/scroll.js
@@ -1,10 +1,10 @@
-document.getElementById("navbar_logo").onclick = function() {
-  scrollToTop();
-}
-
-function scrollToTop() {
-  if (document.body.scrollTop !== 0 || document.documentElement.scrollTop !== 0) {
-        window.scrollBy(0, -40);
-        requestAnimationFrame(scrollToTop);
-    }
-}
+// document.getElementById("navbar_logo").onclick = function() {
+//   scrollToTop();
+// }
+//
+// function scrollToTop() {
+//   if (document.body.scrollTop !== 0 || document.documentElement.scrollTop !== 0) {
+//         window.scrollBy(0, -40);
+//         requestAnimationFrame(scrollToTop);
+//     }
+// }
