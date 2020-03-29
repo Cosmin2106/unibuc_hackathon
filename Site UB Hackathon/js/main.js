c529ef726afef63a0fe69197c755c80e5f648415
Cosmin Aprodu <cosmin.aprodu@gmail.com>
2020-03-29 17:25:02 +0300

    Intro generala si detalii text

--- Site UB Hackathon/js/main.js
+++ Site UB Hackathon/js/main.js
@@ -9,3 +9,23 @@ function scrollNavbar() {
     document.getElementById("navbar_main").classList.remove("navbar-contract");
   }
 }
+
+$("#signup_btn_v").hover(function() {
+  $("#container_1_col_1").width("60%");
+  $("#container_1_col_2").width("40%");
+  $("#signup_btn_c").css("opacity", 0.5);
+}, function() {
+  $("#container_1_col_1").width("50%");
+  $("#container_1_col_2").width("50%");
+  $("#signup_btn_c").css("opacity", 1);
+});
+
+$("#signup_btn_c").hover(function() {
+  $("#container_1_col_1").width("40%");
+  $("#container_1_col_2").width("60%");
+  $("#signup_btn_v").css("opacity", 0.5);
+}, function() {
+  $("#container_1_col_1").width("50%");
+  $("#container_1_col_2").width("50%");
+  $("#signup_btn_v").css("opacity", 1);
+});
--- Site UB Hackathon/js/scroll.js
+++ Site UB Hackathon/js/scroll.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
   $("#navbar_logo").on('click', smoothScroll);
+  $("#signup_btn_main").on('click', smoothScroll);
 });
 
-function smoothScroll(event) {
+function smoothScroll() {
   if (this.hash !== "") {
     var oldHash = this.hash;
+    var mainNavHeight = $("#navbar_main").height() + 64;
 
     $('html, body').animate({
-      scrollTop: $(oldHash).offset().top
+      scrollTop: $(oldHash).offset().top - mainNavHeight
     }, 800, 'swing');
-    window.location.hash = oldHash;
+    window.location.hash = oldHash - mainNavHeight;
   }
 }
