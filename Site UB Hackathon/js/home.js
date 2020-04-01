830a71351a94bce7988ffed5886475c5d1cf463e
Cosmin Aprodu <cosmin.aprodu@gmail.com>
2020-04-02 02:06:07 +0300

    Update motto + bug fixes

--- Site UB Hackathon/js/home.js
+++ Site UB Hackathon/js/home.js
@@ -11,20 +11,30 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 let db = firebase.firestore();
+let vLink = "https://forms.gle/HUqRPQuR22Uf866u6";
+let cLink = "https://forms.gle/FjwpZBwbQJTf2rLn6";
+
 var endDate = "Jan 1, 1970 00:00:00";
 var challengeData = new Array();
+var buttonChosen = 0;
+var loadIsReady = false;
+var globalEndDateV;
+var globalEndDateC;
 
 $("#container_3_cont_counter").addClass("item-gone");
 $("#container_3_cont_challenges").addClass("item-gone");
 
 db.collection("all_data").get().then(function (contDocs) {
   challengeData = contDocs.docs[0].data().challenges;
-  endDate = contDocs.docs[1].data().end_time;
-
-  var contestDate = new Date(endDate);
+  var contestDate = new Date(contDocs.docs[1].data().end_time);
   var distance = contestDate - new Date().getTime();
 
+  globalEndDateV = new Date(contDocs.docs[1].data().signup_end_time_v);
+  globalEndDateC = new Date(contDocs.docs[1].data().signup_end_time_c);
+
+  loadReady();
   $("#container_3_cont_loading").addClass("item-gone");
+
   if (distance <= 0) {
     $("#container_3_cont_challenges").removeClass("item-gone");
     loadChallenges(challengeData);
@@ -95,6 +105,37 @@ function loadChallenges(data) {
   }
 }
 
+function loadReady() {
+  console.log("Firestore OK.");
+  loadIsReady = true;
+
+  if (buttonChosen === 1) {
+    openBtnV();
+  } else if (buttonChosen === 2) {
+    openBtnC();
+  }
+  $("#container_loading").removeClass("container-loading-on");
+  buttonChosen = 0;
+}
+
+function openBtnV() {
+  var dist = globalEndDateV - new Date().getTime();
+  if (dist > 0) {
+    window.open(vLink, "_blank");
+  } else {
+    showAlert();
+  }
+}
+
+function openBtnC() {
+  var dist = globalEndDateC - new Date().getTime();
+  if (dist > 0) {
+    window.open(cLink, "_blank");
+  } else {
+    showAlert();
+  }
+}
+
 $("#signup_btn_v").hover(function() {
   $("#container_1_col_1").width("60%");
   $("#container_1_col_2").width("40%");
@@ -103,6 +144,16 @@ $("#signup_btn_v").hover(function() {
   $("#container_1_col_1").width("50%");
   $("#container_1_col_2").width("50%");
   $("#signup_btn_c").css("opacity", 1);
+});
+$("#signup_btn_v").on('click', function () {
+  if (loadIsReady) {
+    openBtnV();
+  } else {
+    buttonChosen = 1;
+    if (!$("#container_loading").hasClass("container-loading-on")) {
+      $("#container_loading").addClass("container-loading-on");
+    }
+  }
 });
 
 $("#signup_btn_c").hover(function() {
@@ -114,3 +165,20 @@ $("#signup_btn_c").hover(function() {
   $("#container_1_col_2").width("50%");
   $("#signup_btn_v").css("opacity", 1);
 });
+$("#signup_btn_c").on('click', function () {
+  if (loadIsReady) {
+    openBtnC();
+  } else {
+    buttonChosen = 2;
+    if (!$("#container_loading").hasClass("container-loading-on")) {
+      $("#container_loading").addClass("container-loading-on");
+    }
+  }
+});
+
+function showAlert() {
+  $("#alert_no_signup").show();
+  setTimeout(function () {
+    $("#alert_no_signup").hide();
+  }, 3000);
+}
